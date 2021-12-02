849ef7ce3d323447616f6ec438d34040e48e8f28
Maciej Pecherzewski <mpecherzewski@altimetrik.com>
2021-12-02 11:25:55 +0100

    archunit mvc demo

---
+++ mvc-archunit-demo/shared-archunit-test/src/main/java/org/archunit/shared/CustomRules.java
@@ -0,0 +1,21 @@
+package org.archunit.shared;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+
+public class CustomRules {
+
+    public static final ArchCondition<JavaClass> notHaveFieldsAnnotatedWithDeprecated =
+            new ArchCondition<>("should not have a field annotated with @Deprecated") {
+                @Override
+                public void check(JavaClass item, ConditionEvents events) {
+                    if (item.getAllFields().stream()
+                            .anyMatch( field -> field.isAnnotatedWith(Deprecated.class))) {
+                        events.add(SimpleConditionEvent.violated(item, "should not have a field annotated with @Deprecated"));
+                    }
+                }
+            };
+
+}
---
+++ mvc-archunit-demo/shared-archunit-test/src/main/java/org/archunit/shared/SharedArchitectureTest.java
@@ -0,0 +1,21 @@
+package org.archunit.shared;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+@AnalyzeClasses(importOptions = {ImportOption.DoNotIncludeArchives.class, ImportOption.DoNotIncludeJars.class, ImportOption.DoNotIncludeTests.class})
+public class SharedArchitectureTest {
+
+    @ArchTest
+    public static final ArchRule deprecationRule = classes()
+        .should(CustomRules.notHaveFieldsAnnotatedWithDeprecated);
+
+    @ArchTest
+    public static final ArchRule application_should_be_free_of_cycles = slices().matching("..(org).(*)..").should().beFreeOfCycles();
+
+}
